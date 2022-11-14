93cd4484302161e533521308c36ab8d5cce32f65
Kaylerina <118151770+Kaylerina@users.noreply.github.com>
2022-11-14 00:55:09 -0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,60 +1,19 @@
-window.onload = function () {
+document.querySelector('#tip-form').onchange = function(){
+
+    var bill = Number(document.getElementById('billTotal').value);
+    var tip = document.getElementById('tipInput').value;
+    document.getElementById('tipOutput').innerHTML = `${tip}%`;
+    var tipValue = bill * (tip/100)
+    var finalBill = bill + tipValue
+  console.log(finalBill)
+  var tipAmount = document.querySelector('#tipAmount')
+  var totalBillWithTip = document.querySelector('#totalBillWithTip')
   
-    var seconds = 00; 
-    var tens = 00; 
-    var appendTens = document.getElementById("tens")
-    var appendSeconds = document.getElementById("seconds")
-    var buttonStart = document.getElementById('button-start');
-    var buttonStop = document.getElementById('button-stop');
-    var buttonReset = document.getElementById('button-reset');
-    var Interval ;
+  tipAmount.value = tipValue.toFixed(2);
+   totalBillWithTip.value =finalBill.toFixed(2);
   
-    buttonStart.onclick = function() {
-      
-      clearInterval(Interval);
-       Interval = setInterval(startTimer, 10);
-    }
-    
-      buttonStop.onclick = function() {
-         clearInterval(Interval);
-    }
-    
+   //Show Results
   
-    buttonReset.onclick = function() {
-       clearInterval(Interval);
-      tens = "00";
-        seconds = "00";
-      appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-    }
-    
-     
-    
-    function startTimer () {
-      tens++; 
-      
-      if(tens <= 9){
-        appendTens.innerHTML = "0" + tens;
-      }
-      
-      if (tens > 9){
-        appendTens.innerHTML = tens;
-        
-      } 
-      
-      if (tens > 99) {
-        console.log("seconds");
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-      }
-      
-      if (seconds > 9){
-        appendSeconds.innerHTML = seconds;
-      }
-    
-    }
-    
-  
+    document.getElementById('results').style.display='block'
   }
+  
